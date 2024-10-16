1c9527da1ea64a0cf19f9e57d64e5b16924d2c0e
wzy <2049562063@qq.com>
2024-10-16 23:21:59 +0800

    change data type to record

--- src/main/java/com/example/demo/dto/LoginResponse.java
+++ src/main/java/com/example/demo/dto/LoginResponse.java
@@ -1,62 +1,8 @@
 package com.example.demo.dto;
 
 
-public class LoginResponse {
-    int code;
-    String message;
-    long timestamp;
-    Claims data;
-    public LoginResponse() {}
-    public LoginResponse(int code, String message, Claims data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
-    public int getCode() {
-        return code;
-    }
-    public void setCode(int code) {
-        this.code = code;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public long getTimestamp() {
-        return timestamp;
-    }
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
-    public Claims getData() {
-        return data;
-    }
-    public void setData(Claims data) {
-        this.data = data;
-    }
-
-    public static class Claims {
-        String role;
-        String token;
-        public Claims() {}
-        public Claims(String role, String token) {
-            this.role = role;
-            this.token = token;
-        }
-        public String getRole() {
-            return role;
-        }
-        public void setRole(String role) {
-            this.role = role;
-        }
-        public String getToken() {
-            return token;
-        }
-        public void setToken(String token) {
-            this.token = token;
-        }
+public record LoginResponse(int code, String message, long timestamp, Claims data) {
+    public record Claims(String role, String token) {
     }
 }
 
--- src/main/java/com/example/demo/dto/TaskFetchRequestBody.java
+++ src/main/java/com/example/demo/dto/TaskFetchRequestBody.java
@@ -1,24 +1,7 @@
 package com.example.demo.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class TaskFetchRequestBody {
-    Account account;
-
-    public TaskFetchRequestBody() {
-    }
-
-    public TaskFetchRequestBody(String username, String password_hash) {
-        account = new Account(username, password_hash);
-    }
-
-    @JsonProperty("account")
-        // 可选：确保 JSON 属性名与字段名匹配
-    Account getAccount() {
-        return account;
-    }
-
-    void setAccount(Account account) {
-        this.account = account;
+public record TaskFetchRequestBody(Account account) {
+    public TaskFetchRequestBody(String username, String password) {
+        this(new Account(username, password));
     }
 }
