33b2786d087959e4a23ca4a0a6b4f97d1c83e984
yu <yufantianzhou@outlook.com>
2024-10-28 20:52:39 +0800

    添加deviceRepository持久层，并重新实现DeviceDataService

--- src/main/java/com/example/demo/service/device/DeviceDataService.java
+++ src/main/java/com/example/demo/service/device/DeviceDataService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service.device;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import com.example.demo.entity.Device;
+import com.example.demo.repository.DeviceRepository;
 
 
 // FIXME maybe this class could be placed in repository
@@ -10,26 +14,43 @@ import java.util.*;
 @Service
 public class DeviceDataService {
     // TODO replaced with postgresql
-    private final Map<String, String> devices = new HashMap<>();
+    //private final Map<String, String> devices = new HashMap<>();
+    @Autowired
+    private DeviceRepository deviceRepository;
 
-
-    public List<String> listDevices() {
-        return new ArrayList<>(devices.keySet());
+    // public List<String> listDevices() {
+    //   return new ArrayList<>(devices.keySet());
+    // }
+    public List<Device> listDevices() {
+        return deviceRepository.findAll();
     }
 
-    public boolean hasDevice(String deviceId) {
-        return devices.containsKey(deviceId);
+    // public boolean hasDevice(String deviceId) {
+    //     return devices.containsKey(deviceId);
+    // }
+    public boolean hasDevice(Integer deviceId) {
+        return deviceRepository.existsById(deviceId);
     }
 
-    public void addDevice(String deviceId, String data) {
-        devices.put(deviceId, data);
+    // public void addDevice(String deviceId, String data) {
+    //     devices.put(deviceId, data);
+    // }
+    public void addDevice(Device device) {
+        deviceRepository.save(device);
     }
 
-    public String getData(String deviceId) {
-        return devices.get(deviceId);
+    // public String getData(String deviceId) {
+    //     return devices.get(deviceId);
+    // }
+    public Device getData(Integer deviceId) {
+        return deviceRepository.findById(deviceId).orElse(null);
     }
 
-    public void deleteData(String deviceId) {
-        devices.remove(deviceId);
+
+    // public void deleteData(String deviceId) {
+    //     devices.remove(deviceId);
+    // }
+    public void deleteData(Integer deviceId) {
+        deviceRepository.deleteById(deviceId);
     }
 }
