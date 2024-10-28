bcaad99e206f80f9094bfff22d0e1f41f326b5e3
wzy <2049562063@qq.com>
2024-10-28 12:42:03 +0800

    test repository

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.entity.Account;
+import com.example.demo.service.AccountService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	AccountService accountService;
 
 	@Test
-	void contextLoads() {
+	void testRepository() {
+		List<String> names = accountService.listUsername();
+		for (String name : names) {
+			System.out.println(name);
+		}
 	}
 
+	@Test
+	void testInterface() {
+
+	}
 }
